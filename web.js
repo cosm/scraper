1e8cd12516a9bc62e2878262820ed9604a1b6d7d
Owen Davies <git@obduk.com>
2012-05-31 06:45:38 +0000

    Added jsdom to fetch web page

--- web.js
+++ web.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express.createServer(express.logger());
+var jsdom = require('jsdom');
+var jquery = 'http://code.jquery.com/jquery-1.7.2.min.js'
 
-app.get('/', function(request, response) {
-  response.send('hello, world');
+app.get('/fetch', function(request, response) {
+  var url = "https://www.google.co.uk/finance?client=ob&q=NASDAQ:FB";
+  var css_expression = "#ref_296878244325128_l";
+
+  jsdom.env({
+    html: url,
+    scripts: [jquery],
+    done: function(errors, window) {
+      var $ = window.$;
+      response.send($(css_expression).text());
+    }
+  });
 });
 
 var port = process.env.PORT || 8080;
